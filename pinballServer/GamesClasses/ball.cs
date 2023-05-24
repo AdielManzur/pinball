efd59ecc449a6784f1dd1c287ada0d57d68ab1f8
adielproject <adielproject@outlook.co.il>
2023-05-24 20:43:42 +0300

    new ball class

---
+++ pinballServer/GamesClasses/ball.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pinballServer.GamesClasses
+{
+    public class Ball
+    {
+        public Vector2 vector { get; set; }
+        public Point ballPosition { get; set; }
+        public int ballSpeed;
+        public int ballRadius;
+        /*
+        public bool checkCollisionWithWalls(int ScreenWidth, int ScreenHeight)
+        {
+            
+            if (ballPosition.Y == ScreenHeight) //Collision with the lower wall
+            {
+                return true;
+            }
+            if (ballPosition.Y == 0) //Collision with the upper wall
+            {
+                return true;
+            }
+            if (ballPosition.X == ScreenWidth)//Collision with the right wall
+            {
+                return true;
+            }
+            if (ballPosition.X == 0)//Collision with the left wall
+            {
+                return true;
+            }
+            return false;
+        }
+            */
+        public bool checkCollisionWithPlayers()
+        {
+            return true;
+        }
+
+    }
+}
